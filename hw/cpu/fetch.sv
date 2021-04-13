e91b8ec69375d9f17620956d3cedea58214bda7f
Eddie Estevez <eestevez@wisc.edu>
2021-04-13 17:43:26 -0500

    changes so far from Eddie

--- hw/cpu/fetch.sv
+++ hw/cpu/fetch.sv
@@ -1,41 +1,39 @@
 /*
-   CS/ECE 552 Spring '20
+   CS/ECE 552 Spring '21
   
-   Filename        : fetch.v
-   Description     : This is the module for the overall fetch stage of the processor.
+   Filename        : Fetch.v
+   Description     : First State of the 5-Stage Processor
 */
 
-module fetch(instr, addr_out, /* jump_ctrl ,*/ addr_in, rst, clk);
+module fetch(Instruction, addr_out, LastPC, PCInc4, addr_in, rst, clk);
 
-	parameter N = 16;
+	parameter N = 32;
 
-	output[N-1:0] 	instr;
-	output [N-1:0] 	addr_out;
-	// input		jump_ctrl;
-	input [N-1:0]   addr_in;
-	input 		rst;
-	input 		clk;
+	output		[N-1:0] 	Instruction;
+	output reg 	[N-1:0] 	addr_out;
+	output		[N-1:0]		PCInc4;
 
+	input 		[N-1:0]		LastPC;
+	input 		[N-1:0]		addr_in;
+	input					rst;
+	input 					clk;
+	
+	wire 		[N-1:0] 	pcCurrent;
+	wire 					Flush;
+	wire					BranchTaken;
+	wire		[N-1:0] 	BranchOffset;
 
-	// WIRES
-	// wire [N-1:0] pcAddr;
-	wire [N-1:0] pcCurrent;
-
-	// MUX right before the PC counter
-
-	// mux_2_1_16b whichPCMux(.InA(addr0), .InB(addr1), .S(jump_ctrl), .Out(pcAddr));
 	
 	// Register that holds the Program Counter
-	
-	reg_16b pcReg(.q_16(pcCurrent),
-                .clk(clk), .rst(rst), .d_16(addr_in), .writeEn(1'b1));
-	//might need to change writeEn to include halt createdump
+	reg_Nb #(N = 32) pcReg(.q_N(pcCurrent),
+                .clk(clk), .rst(rst), .d_N(addr_in), .writeEn(1'b1));
 
 	// Instruction Memory
-	
-	memory2c instr_mem(.data_out(instr), .data_in(16'h0), .addr(pcCurrent), .enable(1'b1), .wr(1'b0), .createdump(1'b0), .clk(clk), .rst(rst));
+	memory2c instr_mem(.data_out(Instruction), .data_in(16'h0), .addr(pcCurrent), .enable(1'b1), .wr(1'b0), .createdump(1'b0), .clk(clk), .rst(rst));
 
-	// Adder for PC
-	cla_16b adder(.A(pcCurrent), .B(16'h4), .C_in(1'b0), .S(addr_out), .C_out());
+
+	always @(posedge clk) begin
+		addr_out = pcCurrent + 32'h4
+	end
    
 endmodule
