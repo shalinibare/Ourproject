4219645a60750ea27e35c242acd1db5c90d3aa8f
Eddie Estevez <eestevez@wisc.edu>
2021-04-13 19:24:15 -0500

    finished write_back, required testing

--- hw/cpu/write_back.sv
+++ hw/cpu/write_back.sv
@@ -5,18 +5,32 @@
    Description     : This is the module for the overall Write Back stage of the processor.
 */
 module write_back (memToReg, readData, addr, data_output);
-	input memToReg;			// control signal if something from mem is going to a reg
-	input [31:0] readData;		// data coming out of the data mem
-	input [31:0] addr;		// data coming into the data mem as the address
-	output [31:0] data_output;  	// output of the write back
-	output [31:0] data_output;  	// output of the write
+	parameter N = 32;
 
-	// wire [15:0] data_mem_mux;
-	
-	// Mux Right After the Data Mem
-	assign data_output = (memToReg)?(readData):(addr);
-	assign data_output = (memToReg)?(readData):(addr);
+	input	[N-1:0]	ExeOut; 	//  Output from the ALU
+	input	[N-1:0]	MemOut; 	//  MemOut signal is instantiated outside of the CPU, 
+								//  comes from the memory controller. 
 
-	// Second Mux for choosing data that same from somewhere else 
+	input	[N-1:0]	PCInc4; 	//  Indicates if the current PC needs. 
+	input	[4:0]	Rx; 		//  Register number that comes from the Op Code 
+								//  from the current Instruction. 
+	input	[4:0]	LR; 		//  Link Register Address 
+	input	[1:0]	WbDataSel;	//  Selects if Write Back should be enabled or not 
+								//  depending on the Instruction via what data is sent out
+
+	input			WbRegSel; 	//  Selects if Write Back should be enabled or not depending 
+								//  on the Instruction via what register the data should be written to 
+
+	output	[N-1:0]	WbData ; 	//  Write back data to be sent to the Register File within Decode 
+	output	[4:0]	WbReg ; 	//  The register the Write back data should be written to within 
+								//  the Register File in Decode 
+
+	// 2:1 Mux
+	assign WbReg = (WbRegSel)?(Rx):(LR);
+
+	// 4:1 Mux
+	assign WbData = (WbDataSel[1])?( 
+					(WbDataSel[0])?(ExeOut):(MemOut)  
+					):(PCInc4);
    
 endmodule
