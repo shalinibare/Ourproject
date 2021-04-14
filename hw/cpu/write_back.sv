e5b4d778cf30bbe19eb216b9dce388408672651e
Eddie Estevez <eestevez@wisc.edu>
2021-04-13 19:14:37 -0500

    updates

--- hw/cpu/write_back.sv
+++ hw/cpu/write_back.sv
@@ -1,18 +1,20 @@
 /*
-   CS/ECE 552 Spring '20
+   CS/ECE 554 Spring '21
   
-   Filename        : wb.v
+   Filename        : write_back.v
    Description     : This is the module for the overall Write Back stage of the processor.
 */
-module wb (memToReg, readData, addr, data_output);
+module write_back (memToReg, readData, addr, data_output);
 	input memToReg;			// control signal if something from mem is going to a reg
-	input [15:0] readData;		// data coming out of the data mem
-	input [15:0] addr;		// data coming into the data mem as the address
-	output [15:0] data_output;  	// output of the write back
+	input [31:0] readData;		// data coming out of the data mem
+	input [31:0] addr;		// data coming into the data mem as the address
+	output [31:0] data_output;  	// output of the write back
+	output [31:0] data_output;  	// output of the write
 
-	wire [15:0] data_mem_mux;
+	// wire [15:0] data_mem_mux;
 	
 	// Mux Right After the Data Mem
+	assign data_output = (memToReg)?(readData):(addr);
 	assign data_output = (memToReg)?(readData):(addr);
 
 	// Second Mux for choosing data that same from somewhere else 
