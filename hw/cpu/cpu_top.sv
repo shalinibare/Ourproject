9d391ae7bb540eebd88a2441f6b731c759d9c086
cgiesler <cgiesler@wisc.edu>
2021-04-14 23:14:25 -0500

    Connecting modules

--- hw/cpu/cpu_top.sv
+++ hw/cpu/cpu_top.sv
@@ -1,10 +1,20 @@
 module cpu_top(
 	input clk, rst_n, nextTransaction,
 	input [1:0] Interrupt,
-	output ack, en, hlt,
+	output ack, en, halt,
 	output [31:0] memDataOut, memAddr //DMA FSM stuff (tbt)
 );
 
+logic [31:0] RegData0, RegData1, imm, BrPC, SPOut, SPin, exeOut, wData;
+logic [31:0] PC, inst, LastPC;
+logic [31:0] RegData1_o;
+logic [31:0] Addr, MemDataIn, MemOut;
+logic [31:0] WbData;
+logic [4:0] Rx, LR, WbReg;
+logic valid;
+logic Branch, MemInSel, memwr, memrd, WbRegSel, SPwe_o, wEn, wEn_o, SPwe_in;
+logic [4:0] wReg;
+logic [1:0] WbDataSel, ALU_A_SEL, ALU_B_SEL;
 
 //DMA FSM
 assign memDataOut = 32'h0;
@@ -19,31 +29,41 @@ assign ack = 1'b0;
 
 
 //Fetch
-logic [31:0] PC, inst;
 fetch fetch_stage(.*);
 
+//F_D Pipeline
 
 //Decode
-logic [31:0] RegData0, RegData1, imm, BrPC, SPOut;
-logic ALU_A_SEL, Branch, MemInSel, memwr, memrd, WbDataSel, WbRegSel, SPwe_o, wEn, SPwe_in;
-logic ALU_B_SEL;
 
+assign wEn = wEn_o;
+assign SPwe_in = SPwe_o;
+assign SPin = exeOut;
+assign wReg = WbReg;
+assign wData = WbData;
 decode dec_stage(.*);
 
+//D_X Pipeline
 
 //Execute
-logic [31:0] exeOut, RegData1_o;
 execute exe_stage(.RegData0(RegData0), .RegData1(RegData1), .SPOut(SPOut), .imm(imm), .opcode(inst[31:27]),
 				.ALU_A_SEL(ALU_A_SEL), .ALU_B_SEL(ALU_B_SEL), .MemInSel(MemInSel), 
 				.exeOut(exeOut), .RegData1_o(RegData1_o));
 
+//X_M Pipeline
+
 //Mem
+
+assign Addr = exeOut;
+assign MemDataIn = RegData1_o;
 memory mem_stage(.*);
 
+//M_W Pipeline
+
 //Writeback
+
 wb wb_stage(.*);
 
-assign hlt = &inst[31:27];
+assign halt = &inst[31:27];
 
 endmodule
 
